9070a87c97b000b3abccf78976be63114ce4af98
Alisa <cheshyr.alisa@gmail.com>
2020-09-29 11:24:22 +0300

    Updates

---
+++ precache-manifest.fd536d4676bab3ec95edc701d3302632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e1d0cf8c831adbaf2c4be123b530dc",
+    "url": "/Notes_App/index.html"
+  },
+  {
+    "revision": "a8b1797e219611559209",
+    "url": "/Notes_App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75067b6230647b191d9e",
+    "url": "/Notes_App/static/js/2.9f24f43b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Notes_App/static/js/2.9f24f43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b1797e219611559209",
+    "url": "/Notes_App/static/js/main.98b973e6.chunk.js"
+  },
+  {
+    "revision": "4345522fbccb095c64cd",
+    "url": "/Notes_App/static/js/runtime-main.7a035553.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Notes_App/static/media/logo.5d5d9eef.svg"
+  }
+]);
